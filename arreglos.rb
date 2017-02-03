3fddf38023903e2224a2fa4dd03400938e7caa9d
Gerardo Sanchez <yerald231ger@gmail.com>
2017-02-02 22:04:55 -0600

    ok

--- arreglos.rb
+++ arreglos.rb
@@ -1,4 +1,15 @@
 arreglo = ["ok",1]
 
-puts arreglo[0]
-puts arreglo[1]
+arreglo2 = Array.new(4) << [1, "two", 3.0]
+arreglo3 = %w[1 two 3.0 1 gera sanchez hernandez]
+
+puts arreglo2
+print arreglo2
+
+arreglo3.each do |v|
+    print v + " "
+end
+
+arreglo3.each_with_index do |v,x|
+    print v + "#{x} "
+end
---
+++ ranges.rb
@@ -0,0 +1,8 @@
+
+(0..30).step(5).each do |n|
+   puts n
+end
+
+('aa'..'zz').each do |x|
+   print x + ' , '
+end
